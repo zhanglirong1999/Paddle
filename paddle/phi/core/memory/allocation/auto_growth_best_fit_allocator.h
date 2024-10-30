f0399b35f65d4ef93d71ec148ed4ccb7c20a656b
vivienfanghuagood <89012307+vivienfanghuagood@users.noreply.github.com>
2024-10-30 11:52:51 +0800

    remove release lock (#69046)

--- paddle/phi/core/memory/allocation/auto_growth_best_fit_allocator.h
+++ paddle/phi/core/memory/allocation/auto_growth_best_fit_allocator.h
@@ -46,7 +46,8 @@ class AutoGrowthBestFitAllocator : public Allocator {
 
   // Release the memory block which is not used in pool.
   uint64_t ReleaseImpl(const phi::Place &place) override {
-    std::lock_guard<SpinLock> guard(spinlock_);
+    // TODO(vivienfanghuagood): the next line may cause the process to deadlock.
+    // std::lock_guard<SpinLock> guard(spinlock_);
     return FreeIdleChunks();
   }
 
