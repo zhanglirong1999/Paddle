7e07736a888acdd607bfaecda5bd5107bdfb9942
Nyakku Shigure <sigure.qaq@gmail.com>
2024-11-04 10:15:52 +0800

    [Infra][3.13] replace `pkg_resources` with `importlib.resources` (#69130)

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -583,6 +583,7 @@ from .tensor.to_string import set_printoptions
 # CINN has to set a flag to include a lib
 if is_compiled_with_cinn():
     import os
+    from importlib import resources
 
     package_dir = os.path.dirname(os.path.abspath(__file__))
     runtime_include_dir = os.path.join(package_dir, "libs")
@@ -590,10 +591,8 @@ if is_compiled_with_cinn():
     if os.path.exists(cuh_file):
         os.environ.setdefault('runtime_include_dir', runtime_include_dir)
 
-    import pkg_resources
-
-    data_file_path = pkg_resources.resource_filename('paddle.cinn_config', '')
-    os.environ['CINN_CONFIG_PATH'] = data_file_path
+    data_file_path = resources.files('paddle.cinn_config')
+    os.environ['CINN_CONFIG_PATH'] = str(data_file_path)
 
 if __is_metainfo_generated and is_compiled_with_cuda():
     import os
