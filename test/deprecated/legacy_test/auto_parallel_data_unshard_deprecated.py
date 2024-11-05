Fix compuatation computation [fluid_ops] (#69168)

test/deprecated/legacy_test/auto_parallel_data_unshard_deprecated.py
@@ -93,7 +93,7 @@ class TestDataUnshard(unittest.TestCase):
         input_data = np.array(range(2 * 8)).reshape([2, 8]).astype("float32")
         label_data = np.random.randint(0, 10, [2, 8]).astype("float32")
 
-        fetchs = (
+        fetches = (
             [loss.name, 'split@RESHARD.tmp_0']
             if worker_index == 0
             else [loss.name, 'split@RESHARD.tmp_1']
@@ -101,7 +101,7 @@ class TestDataUnshard(unittest.TestCase):
         loss_np, shard_data_np = exe.run(
             distributed_main_program,
             feed={"input": input_data, "label": label_data},
-            fetch_list=fetchs,
+            fetch_list=fetches,
         )
         desired = input_data[worker_index].reshape(shard_data_np.shape)
         np.testing.assert_allclose(shard_data_np, desired)
@@ -172,11 +172,11 @@ class TestDataUnshard(unittest.TestCase):
 
         input_data = np.array(range(8 * 8)).reshape([8, 8]).astype("float32")
         label_data = np.random.randint(0, 10, [8, 8]).astype("float32")
-        fetchs = [loss.name, 'input']
+        fetches = [loss.name, 'input']
         loss_np, shard_data_np = exe.run(
             distributed_main_program,
             feed={"input": input_data, "label": label_data},
-            fetch_list=fetchs,
+            fetch_list=fetches,
         )
 
         desired = input_data.reshape(shard_data_np.shape)

test/legacy_test/test_sigmoid_cross_entropy_with_logits_grad_with_auto_grad.py
@@ -44,7 +44,7 @@ class TestSigmoidCrossEntropyWithLogitsOpGradWithAutoGrad(unittest.TestCase):
             )
         )
 
-        self.lable = np.random.uniform(
+        self.label = np.random.uniform(
             0, 1, (self.batch_size, self.num_classes)
         ).astype("float32")
 
@@ -76,7 +76,7 @@ class TestSigmoidCrossEntropyWithLogitsOpGradWithAutoGrad(unittest.TestCase):
 
         def cal(fn, place):
             x1 = paddle.to_tensor(self.x, stop_gradient=False, place=place)
-            label1 = paddle.to_tensor(self.lable)
+            label1 = paddle.to_tensor(self.label)
             pos_weight1 = paddle.to_tensor(self.pos_weight, place=place)
             res = fn(x1, label1, pos_weight1)
             return res