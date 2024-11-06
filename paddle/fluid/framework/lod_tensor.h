f71ffd326908eaf5a568be0961bd207b4b2b4457
wanghuancoder <wanghuan29@baidu.com>
2024-11-06 10:26:56 +0800

    clear unused lod code (#69176)

--- paddle/fluid/framework/lod_tensor.h
+++ paddle/fluid/framework/lod_tensor.h
@@ -32,15 +32,6 @@ limitations under the License. */
 namespace paddle {
 namespace framework {
 
-// Split phi::DenseTensor and copy to each place specified in places.
-TEST_API std::vector<phi::DenseTensor> SplitLoDTensor(
-    const phi::DenseTensor& src, const std::vector<phi::Place> places);
-
-TEST_API void MergeLoDTensor(
-    phi::DenseTensor* target,
-    const std::vector<const phi::DenseTensor*>& lod_tensors,
-    phi::Place dst_place);
-
 /*
  * LoD is short for Level of Details.
  *
@@ -59,15 +50,6 @@ TEST_API void MergeLoDTensor(
 using LoD = std::vector<phi::Vector<size_t>>;
 
 std::string LoDToString(const LoD& lod);
-
-LoD SliceInLevel(const LoD& in,
-                 size_t level,
-                 size_t elem_begin,
-                 size_t elem_end);
-/*
- * Transform an LoD from relative offsets to absolute offsets.
- */
-TEST_API LoD ToAbsOffset(const LoD& in);
 
 TEST_API bool operator==(const LoD& a, const LoD& b);
 
@@ -88,84 +70,8 @@ TEST_API bool operator==(const LoD& a, const LoD& b);
  */
 
 TEST_API bool CheckLoD(const LoD& in, int tensor_height = -1);
-/*
- * Check whether this absolute lod's format is valid.
- *
- * ATTENTION:
- *   - Empty lod is treated as valid.
- *
- * It will check two things:
- *  1. all the offsets in a level should be ascending(no same items allowed).
- *  2. there should be more than 2 offsets existing in each level.
- *  3. the first offset of each level should be 0, and the last should be the
- *     same(the height of underlying tensor) or `tensor_height` if
- *     tensor_height>0.
- */
-TEST_API bool CheckAbsLoD(const LoD& in, int tensor_height = -1);
 
-/*
- * Expand the `source` to fit the LoD of `lod`. For example, a `source`
- * phi::DenseTensor is
- *  - LoD: [0, 2]
- *  - tensor: [a0, a1]
- * a `lod` is
- *  - LoD: [0 3 5]
- * returns a new phi::DenseTensor
- *  - [a0 a0 a0 a1 a1]
- */
-template <typename T>
-phi::DenseTensor LodExpand(const phi::DenseTensor& source,
-                           const LoD& lod,
-                           size_t level,
-                           const phi::Place& place) {
-  LoD abs_lod = ToAbsOffset(lod);
-  const auto& lod_level = lod[level];
-  size_t num_instances = source.dims()[0];
-
-  // new tensor
-  phi::DenseTensor tensor;
-  tensor.set_lod(lod);
-  auto dims = source.dims();
-  dims[0] = lod_level.back();
-  tensor.Resize(dims);
-  tensor.mutable_data<T>(place);
-
-  PADDLE_ENFORCE_EQ(
-      num_instances,
-      lod_level.size() - 1,
-      common::errors::InvalidArgument(
-          "The input phi::DenseTensor instance number should be equal to the "
-          "LoD "
-          "level size minus 1."
-          "The input instance number is %zu, LoD level size is %zu.",
-          num_instances,
-          lod_level.size()));
-  for (size_t ins = 0; ins < num_instances; ins++) {
-    for (size_t elem = lod_level[ins]; elem < lod_level[ins + 1]; elem++) {
-      auto slice = tensor.Slice(elem, elem + 1);
-      TensorCopy(source.Slice(ins, ins + 1),
-                 phi::CPUPlace(),
-                 phi::CPUContext(),
-                 &slice);
-    }
-  }
-  return tensor;
-}
-
-// Get the absolute offset of a lod[start_level][start_idx:end_idx] and
-// relative length of details for every levels(i.e., [start_level: ]).
-//
-// For example,
-//   lod = [[0, 3, 4, 8], [0, 9, 10, 11, 13, 17, 19, 22, 24]]
-//   start_level = 0
-//   start_idx = 1
-//   end_idx = 3
-//
-// Returns:
-//  LoD = [[1, 4], [2, 4, 2, 3, 2]]
-//  pair<size_t, size_t> = {11, 24}
-TEST_API std::pair<LoD, std::pair<size_t, size_t>> GetSubLoDAndAbsoluteOffset(
-    const LoD& lod, size_t start_idx, size_t end_idx, size_t start_level);
+TEST_API LoD ConvertToOffsetBasedLoD(const LoD& length_lod);
 
 /*
  * Serialize/Deserialize phi::DenseTensor to std::ostream
@@ -183,8 +89,6 @@ void DeserializeFromStream(std::istream& is,
                            const phi::DeviceContext& dev_ctx,
                            const size_t& seek,
                            const std::vector<int64_t>& shape);
-
-TEST_API LoD ConvertToOffsetBasedLoD(const LoD& length_lod);
 
 void SerializeToStream(std::ostream& os, const phi::DenseTensor& tensor);
 
--- test/cpp/fluid/framework/lod_tensor_test.cc
+++ test/cpp/fluid/framework/lod_tensor_test.cc
@@ -49,46 +49,6 @@ TEST(LoD, data) {
   }
 }
 
-TEST(LoD, ExpandLoD) {
-  LoD lod{{0, 2}};
-  phi::DenseTensor tensor;
-  tensor.set_lod(lod);
-  tensor.Resize({2, 1});
-  tensor.mutable_data<float>(phi::CPUPlace());
-  tensor.data<float>()[0] = 0;
-  tensor.data<float>()[1] = 1;
-
-  LoD target;
-  target.emplace_back(std::vector<size_t>{0, 3, 5});
-  auto new_tensor = LodExpand<float>(tensor, target, 0UL, phi::CPUPlace());
-  std::vector<int> result{{0, 0, 0, 1, 1}};
-  for (size_t i = 0; i < 5; i++) {
-    ASSERT_EQ(new_tensor.data<float>()[i], result[i]);
-  }
-}
-
-TEST(LoD, GetFineGrainedLoDLength) {
-  LoD lod;
-  lod.push_back(std::vector<size_t>({0, 2, 4, 5}));
-  lod.push_back(std::vector<size_t>({0, 1, 6, 8, 10, 11}));
-  lod.push_back(
-      std::vector<size_t>({0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26, 29}));
-
-  auto lod_and_offset =
-      paddle::framework::GetSubLoDAndAbsoluteOffset(lod, 1, 2, 0);
-  LoD lod_length = lod_and_offset.first;
-  size_t start_offset = lod_and_offset.second.first;
-  size_t end_offset = lod_and_offset.second.second;
-
-  LoD expected;
-  expected.push_back(std::vector<size_t>{2});
-  expected.push_back(std::vector<size_t>{2, 2});
-  expected.push_back(std::vector<size_t>{2, 3, 4, 2});
-  EXPECT_EQ(lod_length, expected);
-  EXPECT_EQ(start_offset, 15UL);
-  EXPECT_EQ(end_offset, 26UL);
-}
-
 TEST(LoD, AppendLoD) {
   LoD lod_lens;
   lod_lens.push_back(std::vector<size_t>({2}));
@@ -108,114 +68,6 @@ TEST(LoD, AppendLoD) {
   expected.push_back(
       std::vector<size_t>({0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26}));
   EXPECT_EQ(origin, expected);
-}
-
-TEST(LoD, ToAbsOffset) {
-  LoD relative_lod;
-  relative_lod.push_back(std::vector<size_t>({0, 2}));
-  relative_lod.push_back(std::vector<size_t>({0, 1, 3}));
-  relative_lod.push_back(std::vector<size_t>({0, 2, 4, 5}));
-
-  LoD abs_lod = paddle::framework::ToAbsOffset(relative_lod);
-
-  LoD expected;
-  expected.push_back(std::vector<size_t>({0, 5}));
-  expected.push_back(std::vector<size_t>({0, 2, 5}));
-  expected.push_back(std::vector<size_t>({0, 2, 4, 5}));
-
-  EXPECT_EQ(abs_lod, expected);
-}
-
-TEST(LoD, SplitLoDTensor) {
-  LoD lod;
-  lod.push_back(std::vector<size_t>({0, 2, 4, 5, 6}));
-  lod.push_back(std::vector<size_t>({0, 1, 6, 8, 13, 15, 20}));
-
-  phi::CPUPlace place;
-  phi::DenseTensor lod_tensor;
-  lod_tensor.Resize({20, 1});
-  float* dst_ptr = lod_tensor.mutable_data<float>(place);
-  for (int i = 0; i < lod_tensor.numel(); ++i) {
-    dst_ptr[i] = static_cast<float>(i);
-  }
-  lod_tensor.set_lod(lod);
-
-  std::vector<phi::Place> places{phi::CPUPlace(), phi::CPUPlace()};
-  LoD lod0;
-  lod0.push_back(std::vector<size_t>({0, 2, 4}));
-  lod0.push_back(std::vector<size_t>({0, 1, 6, 8, 13}));
-  LoD lod1;
-  lod1.push_back(std::vector<size_t>({0, 1, 2}));
-  lod1.push_back(std::vector<size_t>({0, 2, 7}));
-
-  auto lods = SplitLoDTensor(lod_tensor, places);
-  EXPECT_EQ(lods[0].lod(), lod0);
-  EXPECT_EQ(lods[1].lod(), lod1);
-}
-
-TEST(LoD, SplitLoDTensorWithZeroBatchSize) {
-  LoD lod;
-  lod.push_back(std::vector<size_t>({0}));
-
-  phi::CPUPlace place;
-  phi::DenseTensor lod_tensor;
-  lod_tensor.Resize({0, 5});
-  lod_tensor.mutable_data<float>(place);
-  lod_tensor.set_lod(lod);
-
-  std::vector<phi::Place> places{phi::CPUPlace(), phi::CPUPlace()};
-  LoD lod_res;
-  lod_res.push_back(std::vector<size_t>({0}));
-
-  auto lods = SplitLoDTensor(lod_tensor, places);
-  EXPECT_EQ(lods[0].lod(), lod_res);
-  EXPECT_EQ(lods[1].lod(), lod_res);
-}
-
-TEST(LoD, MergeLoDTensor) {
-  LoD lod;
-  lod.push_back(std::vector<size_t>({0, 2, 4, 5, 6}));
-  lod.push_back(std::vector<size_t>({0, 1, 6, 8, 13, 15, 20}));
-
-  phi::CPUPlace place;
-
-  phi::DenseTensor lod_tensor0;
-  LoD lod0;
-  lod0.push_back(std::vector<size_t>({0, 2, 4}));
-  lod0.push_back(std::vector<size_t>({0, 1, 6, 8, 13}));
-  lod_tensor0.set_lod(lod0);
-
-  lod_tensor0.Resize({13, 1});
-  float* dst_ptr = lod_tensor0.mutable_data<float>(place);
-  for (int i = 0; i < lod_tensor0.numel(); ++i) {
-    dst_ptr[i] = static_cast<float>(i);
-  }
-
-  phi::DenseTensor lod_tensor1;
-  LoD lod1;
-  lod1.push_back(std::vector<size_t>({0, 1, 2}));
-  lod1.push_back(std::vector<size_t>({0, 2, 7}));
-  lod_tensor1.set_lod(lod1);
-  lod_tensor1.Resize({7, 1});
-  dst_ptr = lod_tensor1.mutable_data<float>(place);
-  for (int i = 0; i < lod_tensor1.numel(); ++i) {
-    dst_ptr[i] = static_cast<float>(i);
-  }
-
-  phi::DenseTensor lod_tensor2;
-  LoD lod2;
-  lod2.push_back(std::vector<size_t>({0}));
-  lod2.push_back(std::vector<size_t>({0}));
-  lod_tensor2.set_lod(lod2);
-  lod_tensor2.Resize({0});
-  dst_ptr = lod_tensor2.mutable_data<float>(place);
-
-  std::vector<const phi::DenseTensor*> lods{
-      &lod_tensor0, &lod_tensor1, &lod_tensor2};
-
-  phi::DenseTensor lod_tensor;
-  MergeLoDTensor(&lod_tensor, lods, place);
-  EXPECT_EQ(lod_tensor.lod(), lod);
 }
 
 TEST(LoD, CheckLoD) {
@@ -247,29 +99,6 @@ TEST(LoD, CheckLoD) {
   ASSERT_TRUE(CheckLoD({{0, 1, 2, 3, 4, 5}}, 5));
   ASSERT_TRUE(CheckLoD({{0, 1, 3, 3, 4, 5}}, 5));
   ASSERT_FALSE(CheckLoD({{0, 1, 3, 2, 5}}, 5));
-}
-
-TEST(LoD, CheckAbsLoD) {
-  LoD relative_lod;
-  relative_lod.push_back(std::vector<size_t>({0, 2}));
-  relative_lod.push_back(std::vector<size_t>({0, 1, 3}));
-  relative_lod.push_back(std::vector<size_t>({0, 2, 4, 5}));
-
-  auto abs_lod = ToAbsOffset(relative_lod);
-
-  ASSERT_TRUE(CheckAbsLoD(abs_lod));
-
-  // check less than 2 offsets in a level.
-
-  // check the last item should be compatible with tensor height.
-  abs_lod.back().back()++;
-  ASSERT_FALSE(CheckAbsLoD(abs_lod));
-  abs_lod.back().back()--;  // restore
-
-  // check less than 2 offsets in a lod.
-  LoD abs_lod0;
-  abs_lod0.push_back(std::vector<size_t>({0}));
-  ASSERT_FALSE(CheckAbsLoD(abs_lod0));
 }
 
 TEST(LoD, ConvertToLengthBasedLoD) {
