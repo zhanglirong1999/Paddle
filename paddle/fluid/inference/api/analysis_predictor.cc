418887cbc68c763424377f9c68ed7fa5d627ab73
vivienfanghuagood <89012307+vivienfanghuagood@users.noreply.github.com>
2024-11-29 17:15:26 +0800

    add_cpu_perf (#69819)

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -418,13 +418,15 @@ bool AnalysisPredictor::Init(
     const std::shared_ptr<framework::Scope> &parent_scope,
     const std::shared_ptr<framework::ProgramDesc> &program) {
   VLOG(3) << "Predictor::init()";
-#ifdef PADDLE_WITH_NVTX
+
   if (config_.with_profile_) {
     LOG(WARNING) << "Profiler is activated, which might affect the performance";
+#ifdef PADDLE_WITH_NVTX
     platform::CudaProfilerStart();
     platform::NvprofEnableRecordEvent();
-  }
 #endif
+    platform::EnableProfiler(platform::ProfilerState::kAll);
+  }
 
   if (!status_is_cloned_) {
     root_predictor_id_ = predictor_id_;
@@ -2965,12 +2967,16 @@ AnalysisPredictor::~AnalysisPredictor() {  // NOLINT
     SaveTrtCalibToDisk();
   }
 #endif
-#ifdef PADDLE_WITH_NVTX
+
   if (config_.with_profile_) {
+#ifdef PADDLE_WITH_NVTX
     platform::NvprofDisableRecordEvent();
     platform::CudaProfilerStop();
-  }
 #endif
+    platform::DisableProfiler(platform::EventSortingKey::kTotal,
+                              "./profile.log");
+  }
+
   if (sub_scope_) {
     if (framework::global_transfer_scope_key().find(sub_scope_) !=
         framework::global_transfer_scope_key().end()) {
