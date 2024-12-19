2aa2d7c1a5e83828ee6cd96a35c796dc04ad1f88
chen2016013 <111894720+chen2016013@users.noreply.github.com>
2024-12-19 14:45:23 +0800

    fix maximum recursion depth exceeded bug (#70320)

--- python/paddle/decomposition/recompute.py
+++ python/paddle/decomposition/recompute.py
@@ -1092,19 +1092,20 @@ def find_parent_ops(value):
 
     def _find_parent_ops(value):
         parent_ops = set()
-        if value in visited:
-            return parent_ops
-        visited.add(value)
-        parent_op = value.get_defining_op()
-        if parent_op is not None:
-            parent_ops.add(parent_op)
-        else:
-            return parent_ops
-        op_inputs = parent_op.operands_source()
-        for op_input in op_inputs:
-            if not value.initialized():
+        stack = [value]
+
+        while stack:
+            current = stack.pop()
+            if current in visited:
                 continue
-            parent_ops = parent_ops | _find_parent_ops(op_input)
+            visited.add(current)
+            parent_op = current.get_defining_op()
+            if parent_op is not None:
+                parent_ops.add(parent_op)
+                op_inputs = parent_op.operands_source()
+                for op_input in op_inputs:
+                    if current.initialized():
+                        stack.append(op_input)
         return parent_ops
 
     return _find_parent_ops(value)
