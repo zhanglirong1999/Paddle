421c69c9b051fb3d1cdc03e2316736d9f6c9fec1
Shi Kai <albre02@outlook.com>
2024-12-25 10:13:53 +0800

    [CINN][Add Backend Pass Comment No.3] Add comment for extern_call_process (#70233)
    * Add comment for extern_call_process.h
    
    * Fix codestyle

--- paddle/cinn/optim/extern_call_process.h
+++ paddle/cinn/optim/extern_call_process.h
@@ -19,8 +19,45 @@
 namespace cinn {
 namespace optim {
 
+/**
+ * Replace the external call statements that are Store operations involving
+ * multiple output with the external call themselves.
+ *
+ * This pass is applicable in scenarios where the external call statements
+ * containing Store operations that have multiple output arguments. Replacing
+ * them can simplify the IR.
+ *
+ * When applied, this pass will traverse the statements within block and
+ identify
+ * the external call statements which are Store operations with multiple output.
+ * For each matching Store operation, this pass replaces the entire Store
+ expression
+ * with the external call expression itself.
+
+ * Examples:
+ * 1. Multi-Output External Call:
+ *    Input IR:
+ *      Store(target, Call(extern_func, args, write_args))
+ *    Output IR:
+ *      Call(extern_func, args, write_args)
+ *
+ * 2. Single-Output External Call (No Change):
+ *    Input IR:
+ *      Store(target, Call(extern_func, args, {}))
+ *    Output IR:
+ *      Store(target, Call(extern_func, args, {}))
+ */
 void ExternCallMultiOutputShallowStore(Expr* e);
 
+/*
+ * Remove external call statements that are TupleGet.
+ *
+ * This pass is applicable in scenarios where the external call statements are
+ * TupleGet.
+ *
+ * When applied, this pass will traverse the external call statements in the
+ * block and remove the statements that are TupleGet.
+ */
 void ExternCallRemoveTupleGetStatements(Expr* e);
 
 }  // namespace optim
