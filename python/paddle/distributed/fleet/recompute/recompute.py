e4b882367a3da17bb28bc2a37f27d5b5e97cbcd3
sneaxiy <32832641+sneaxiy@users.noreply.github.com>
2024-12-26 14:03:03 +0800

    Remove storage when recompute using saved_tensor_hooks (#70455)
    * Remove storage when recompute using saved_tensor_hooks
    
    * refine codes

--- python/paddle/distributed/fleet/recompute/recompute.py
+++ python/paddle/distributed/fleet/recompute/recompute.py
@@ -430,7 +430,7 @@ def _recompute_without_reentrant(
                             with paddle.autograd.saved_tensors_hooks(
                                 inner_pack, inner_unpack
                             ):
-                                unused_outputs = function(*args, **kwargs)
+                                function(*args, **kwargs)
             else:
                 with paddle.set_grad_enabled(True), paddle.amp.auto_cast(
                     enable=is_fw_autocast,
@@ -441,14 +441,14 @@ def _recompute_without_reentrant(
                 ), paddle.autograd.saved_tensors_hooks(
                     inner_pack, inner_unpack
                 ):
-                    unused_outputs = function(*args, **kwargs)
+                    function(*args, **kwargs)
 
         if x not in storage:
             raise Exception(
                 "Not supported to retrieve a tensor saved by autograd multiple times that is no need to recompute."
             )
 
-        return storage[x]
+        return storage.pop(x)
 
     with paddle.autograd.saved_tensors_hooks(pack, unpack):
         outputs = function(*args, **kwargs)
