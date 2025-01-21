2b49415204239cbfd4ad94003a667ee207f7b5f7
Junjie Zhang <1356732652@qq.com>
2025-01-21 18:54:20 +0800

    fixbugs (#70920)

--- test/tensorrt/test_converter_activation.py
+++ test/tensorrt/test_converter_activation.py
@@ -281,7 +281,10 @@ class TestThresholdedReluTRTPattern(TensorRTBaseTest):
         self.max_shape = {"x": [5, 3]}
 
     def test_trt_result(self):
-        self.check_trt_result()
+        self.check_trt_result(rtol=1e-3, atol=1e-3)
+
+    def test_trt_result_fp16(self):
+        self.check_trt_result(rtol=1e-3, atol=1e-3, precision_mode="fp16")
 
 
 class TestMishCase1TRTPattern(TensorRTBaseTest):
